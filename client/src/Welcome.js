0ced1b499f2b985ce3d826293c5ab9b06b416ff3
Maryann Dodi <maryann.mbn@gmail.com>
2022-12-04 14:44:08 -0500

    Remove useEffect from welcome component

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -9,42 +9,6 @@ const {user} = useAuth0();
 const navigate = useNavigate();
 const {actions:{getUser,addUser},state} = useContext(UserContext);
 
-useEffect(()=>{
-    if(user){
-        try{
-        fetch('/find-user', {
-                    method: 'POST',
-    
-                    body: JSON.stringify({email:user.email}),
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                    },
-                    })
-            .then((response) => response.json())
-            .then(data =>{ if(data.status === 200)
-                {getUser(data.data)
-                }else{
-                    fetch('/add-user', {
-                                method: 'POST',
-                
-                                body: JSON.stringify({state}),
-                                headers: {
-                                    'Content-type': 'application/json; charset=UTF-8',
-                                },
-                                })
-                        .then((response) => response.json())
-                        .then(data => getUser(data.data))
-                            console.log('in use effect')}
-            })
-            
-           
-        }catch(err){
-                console.log(err)
-            }
-        }
-    //     // else{
-        //   
-        },[])
     return(
         <StyledWelcome>
             <h1>Welcome, {user.given_name}</h1>
